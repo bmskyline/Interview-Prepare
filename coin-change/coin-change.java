3d8a11daf2b4130bb78ad6bd0e46704d663a2915
bmskyline <tienmanhbmt@gmail.com>
2021-11-03 11:09:16 +0700

    Time: 52 ms (16.97%), Space: 38.5 MB (70.13%) - LeetHub

---
+++ coin-change/coin-change.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0)
+            return 0;
+        int[] dp = new int[amount+1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int x : coins) {
+                if (i >= x && dp[i - x] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i], dp[i - x] +1);
+            }
+        }
+        System.out.println(Arrays.toString(dp));
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+    /*
+    Time complexity : O(n)
+    Space complexity : O(n)
+    */
+}
