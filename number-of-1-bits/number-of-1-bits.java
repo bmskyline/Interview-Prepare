7629a9a2d9aea6bb2e9c0e8ef21e1a0c70b16afb
bmskyline <tienmanhbmt@gmail.com>
2021-11-05 08:57:53 +0700

    Time: 0 ms (100.00%), Space: 36 MB (64.48%) - LeetHub

---
+++ number-of-1-bits/number-of-1-bits.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) == 1)
+                count++;
+            n = n >> 1;
+        }
+        return count;
+    }
+    /*
+    Time complexity : O(1)
+    Space complexity : O(1)
+    */
+}
