550fdc9dc8b6bee8c94f8da64a96cfa8b465eb7b
bmskyline <tienmanhbmt@gmail.com>
2021-11-05 08:48:05 +0700

    Time: 5 ms (46.44%), Space: 36.3 MB (52.22%) - LeetHub

---
+++ arranging-coins/arranging-coins.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        int i = 1;
+        while (n >= i) {
+            n -= i;
+            i++;
+        }
+        return i - 1;
+    }
+    /*
+    Time complexity : O(n)
+    Space complexity : O(1)
+    */
+}
