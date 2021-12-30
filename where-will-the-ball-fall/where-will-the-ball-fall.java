a52523ea91dddc33d0a5d8f5aebbcf684327888c
bmskyline <tienmanhbmt@gmail.com>
2021-12-30 10:10:00 +0700

    Time: 1 ms (69.24%), Space: 40.4 MB (48.42%) - LeetHub

--- where-will-the-ball-fall/where-will-the-ball-fall.java
+++ where-will-the-ball-fall/where-will-the-ball-fall.java
@@ -1,36 +1,35 @@
 class Solution {
     public int[] findBall(int[][] grid) {
         int[] res = new int[grid[0].length];
-        Map<Integer, Integer> dp = new HashMap();
+        Integer[][] dp = new Integer[grid.length][grid[0].length];
         for (int i = 0; i < res.length; i++) {
             res[i] = dfs(grid, 0, i, dp);
         }
         return res;
     }
     
-    int dfs(int[][] grid, int row, int col, Map<Integer, Integer> dp) {
+    int dfs(int[][] grid, int row, int col, Integer[][] dp) {
         if (row == grid.length)
             return col;
-        int key = row * grid[0].length + col;
-        if (dp.containsKey(key))
-            return dp.get(key);
+        if (dp[row][col] != null)
+            return dp[row][col];
         if (grid[row][col] == 1) {
             if (col == grid[0].length - 1) {
-                dp.put(key, -1);
+                dp[row][col] = -1;
             } else {
                 if (grid[row][col + 1] == -1)
-                    dp.put(key, -1);
-                else dp.put(key, dfs(grid, row + 1, col + 1, dp));
+                    dp[row][col] = -1;
+                else dp[row][col] = dfs(grid, row + 1, col + 1, dp);
             }
         } else {
             if (col == 0) {
-                dp.put(key, -1);
+                dp[row][col] = -1;
             } else {
                 if (grid[row][col - 1] == 1)
-                    dp.put(key, -1);
-                else dp.put(key, dfs(grid, row + 1, col - 1, dp));
+                    dp[row][col] = -1;
+                else dp[row][col] = dfs(grid, row + 1, col - 1, dp);
             }
         }
-        return dp.get(key);
+        return dp[row][col];
     }
 }
