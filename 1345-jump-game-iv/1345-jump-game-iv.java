36c00c7d6c0343a74b784d5757b055a8997104fd
bmskyline <tienmanhbmt@gmail.com>
2022-01-15 09:25:41 +0700

    Time: 191 ms (9.09%), Space: 122.4 MB (18.75%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minJumps(int[] arr) {
+        /*
+        100 4
+        -23 2
+        404 9
+        23 7
+        3 8
+        
+        100 -> 4
+        i-1 = 404 -> 9
+        i+1 = 23 -> 7
+        
+        1 2 3 4 5
+        */
+        int n = arr.length;
+        HashMap<Integer, List<Integer>> indicesOfValue = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            indicesOfValue.computeIfAbsent(arr[i], x -> new LinkedList<>()).add(i);
+        boolean[] visited = new boolean[n]; visited[0] = true;
+        Queue<Integer> q = new LinkedList<>(); q.offer(0);
+        int step = 0;
+        while (!q.isEmpty()) {
+            for (int size = q.size(); size > 0; --size) {
+                int i = q.poll();
+                if (i == n - 1) return step; // Reached to last index
+                List<Integer> next = indicesOfValue.get(arr[i]);
+                next.add(i - 1); next.add(i + 1);
+                for (int j : next) {
+                    if (j >= 0 && j < n && !visited[j]) {
+                        visited[j] = true;
+                        q.offer(j);
+                    }
+                }
+                next.clear(); // avoid later lookup indicesOfValue arr[i]
+            }
+            step++;
+        }
+        return 0;
+    }
+}
