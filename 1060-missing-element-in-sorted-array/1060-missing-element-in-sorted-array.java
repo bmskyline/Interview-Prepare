490e233c1684971f8e6bbef566a95405229c3efc
Manh Nguyen <tienmanhbmt@gmail.com>
2022-03-04 15:31:12 +0700

    Time: 0 ms (100.00%), Space: 59 MB (5.80%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.java
@@ -6,6 +6,8 @@ class Solution {
         0 2 3 5-2
         5
         */
+        if (k > count(nums, nums.length - 1))
+          return nums[nums.length - 1] + k - count(nums, nums.length - 1);
         int l = 0;
         int r = nums.length - 1;
         while (l < r) {
@@ -16,15 +18,7 @@ class Solution {
                 l = m + 1;
             }
         }
-        int t = count(nums, l);
-        if (t >= k)
-            l = l - 1;
-        t = count(nums, l);
-        while (t < k) {
-            nums[l]++;
-            t++;
-        }
-        return nums[l];
+        return nums[l-1] + k - count(nums, l - 1);
     }
     
     int count(int[] nums, int m) {
