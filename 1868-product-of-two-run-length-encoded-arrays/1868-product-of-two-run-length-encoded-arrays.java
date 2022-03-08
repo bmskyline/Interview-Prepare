5b088735e2a15df50f9666ff8e2afd7ac173a5c8
Manh Nguyen <tienmanhbmt@gmail.com>
2022-03-08 10:44:38 +0700

    Time: 47 ms (41.67%), Space: 219.3 MB (45.45%) - LeetHub

--- 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
+++ 1868-product-of-two-run-length-encoded-arrays/1868-product-of-two-run-length-encoded-arrays.java
@@ -8,41 +8,24 @@ class Solution {
         i = 0
         
         */
-        int remain1 = encoded1[0][1];
-        int remain2 = encoded2[0][1];
-        int num1 = encoded1[0][0];
-        int num2 = encoded2[0][0];
-        int idx1 = 1;
-        int idx2 = 1;
-        List<List<Integer>> res = new ArrayList();
-        while (remain1 > 0 && remain2 > 0) {
-            List<Integer> list = new ArrayList();
-            list.add(num1 * num2);
-            int min = Math.min(remain1, remain2);
-            list.add(min);
-            if (res.isEmpty()) {
-                res.add(list);
-            } else {
-                List<Integer> last = res.get(res.size() - 1);
-                if (list.get(0).equals(last.get(0))) {
-                    last.set(1, last.get(1) + list.get(1));
-                    res.set(res.size() - 1, last);
-                } else {
-                    res.add(list);
-                }
-            }
-            remain1 -= min;
-            remain2 -= min;
-            if (remain1 == 0 && idx1 < encoded1.length) {
-                remain1 = encoded1[idx1][1];
-                num1 = encoded1[idx1][0];
+        int idx1 = 0, idx2 = 0;
+        List<List<Integer>> res = new ArrayList<>();
+		
+        while (idx1 < encoded1.length) {
+            int len = Math.min(encoded1[idx1][1], encoded2[idx2][1]);
+            int mult = encoded1[idx1][0] * encoded2[idx2][0];
+            
+            if (res.size() > 0 && res.get(res.size() - 1).get(0) == mult)
+                res.get(res.size() - 1).set(1, res.get(res.size() - 1).get(1)+len);
+            else
+                res.add(Arrays.asList(mult, len));
+            
+			encoded1[idx1][1] -= len;
+            encoded2[idx2][1] -= len;
+            if(encoded1[idx1][1] == 0) 
                 idx1++;
-            }
-            if (remain2 == 0 && idx2 < encoded2.length) {
-                remain2 = encoded2[idx2][1];
-                num2 = encoded2[idx2][0];
+            if(encoded2[idx2][1] == 0) 
                 idx2++;
-            }
         }
         return res;
     }
