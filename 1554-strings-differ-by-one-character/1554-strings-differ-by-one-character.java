508eec5ff0a256ac6307ecb81a88c804ab269bd7
Manh Nguyen <tienmanhbmt@gmail.com>
2022-03-08 21:40:55 +0700

    Time: 41 ms (91.72%), Space: 54 MB (83.21%) - LeetHub

--- 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.java
+++ 1554-strings-differ-by-one-character/1554-strings-differ-by-one-character.java
@@ -1,14 +1,24 @@
 class Solution {
     public boolean differByOne(String[] dict) {
-        Set<String> set = new HashSet();
-        for (int i = 0; i < dict[0].length(); i++) {
-            for (String s : dict) {
-                StringBuilder cut = new StringBuilder(s).deleteCharAt(i);
-                if (set.contains(cut.toString()))
-                    return true;
-                else set.add(cut.toString());
+        long[] hash = new long[dict.length];
+        long MOD = (long) Math.pow(10, 11) + 7;
+        for (int i = 0; i < dict.length; i++) {
+            for (int j = 0; j < dict[i].length(); j++) {
+                hash[i] = (hash[i] * 26 + dict[i].charAt(j)) % MOD;
             }
+        }
+        long mult = 1;
+        Set<Long> set = new HashSet();
+        for (int j = dict[0].length() - 1; j >= 0; --j) {
             set.clear();
+            for (int i = 0; i < dict.length; i++) {
+                long h = (MOD + hash[i] - (long)mult * (dict[i].charAt(j) - 'a') % MOD) % MOD;
+                if (set.contains(h)) {
+                    return true;
+                }
+                set.add(h);
+            }
+            mult = 26 * mult % MOD;
         }
         return false;
     }
