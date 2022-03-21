fdd2e426f5bdc3f4700bb1904105c1fceb97b3b8
Manh Nguyen <tienmanhbmt@gmail.com>
2022-03-21 10:21:34 +0700

    Time: 4 ms (32.02%), Space: 42.8 MB (11.57%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.java
+++ 791-custom-sort-string/791-custom-sort-string.java
@@ -2,7 +2,6 @@ class Solution {
     public String customSortString(String order, String s) {
         /*
         f c b a d t
-        1 0 2 4 3 2
         fre : aabbdaddfa
         priority 
         a 4
@@ -13,24 +12,17 @@ class Solution {
         
         f bb aaaadddddd
         */
-        int[] fre = new int[26];
         int[] pos = new int[26];
         for (int i = 0; i < order.length(); i++) {
             pos[order.charAt(i) - 'a'] = i + 1;
         }
         PriorityQueue<Character> q = new PriorityQueue<>((a, b) -> pos[a - 'a'] - pos[b - 'a']);
         for (int i = 0; i < s.length(); i++) {
-            if (fre[s.charAt(i) - 'a'] == 0)
-                q.offer(s.charAt(i));
-            fre[s.charAt(i) - 'a']++;
+            q.offer(s.charAt(i));
         }
         StringBuilder res = new StringBuilder();
         while (!q.isEmpty()) {
-            char c = q.poll();
-            while (fre[c - 'a'] > 0) {
-                res.append(c);
-                fre[c - 'a']--;
-            }
+            res.append(q.poll());
         }
         return res.toString();
     }
