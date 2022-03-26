3896138fe848f5e76c23e87379501357ec727f98
Manh Nguyen <tienmanhbmt@gmail.com>
2022-03-26 08:11:42 +0700

    Time: 0 ms (100.00%), Space: 54.1 MB (57.41%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] == target) {
+                return m;
+            } else if (nums[m] > target) {
+                r = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return nums[l] == target ? l : -1;
+    }
+}
