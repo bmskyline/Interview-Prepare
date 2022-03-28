3d94bff0a9ed86f7155c929ec155cc9a51b1c910
Manh Nguyen <tienmanhbmt@gmail.com>
2022-03-28 09:49:46 +0700

    Time: 249 ms (25.14%), Space: 117.6 MB (36.43%) - LeetHub

--- 282-expression-add-operators/282-expression-add-operators.java
+++ 282-expression-add-operators/282-expression-add-operators.java
@@ -9,30 +9,25 @@ class Solution {
         12
         */
         List<String> res = new ArrayList();
-        generate(res, num, target, 0, 0, 0, new StringBuilder());
+        generate(res, num, target, 0, 0, 0, "");
         return res;
     }
     
-    void generate(List<String> res, String s, int target, int i, long pre, long value, StringBuilder path) {
+    void generate(List<String> res, String s, int target, int i, long pre, long value, String path) {
         if (i == s.length()) {
-            if (value == target) res.add(path.toString());
+            if (value == target) res.add(path);
             return;
         }
         for (int j = i; j < s.length(); j++) {
             if (j > i && s.charAt(i) == '0') 
                 break;
             long num = Long.parseLong(s.substring(i, j + 1));
-            int cur = path.length();
             if (i == 0) {
-                generate(res, s, target, j + 1, num, num, path.append(num));
-                path.delete(cur, path.length());
+                generate(res, s, target, j + 1, num, num, path + num);
             } else {
-                generate(res, s, target,j + 1, num, value + num, path.append("+").append(num));
-                path.delete(cur, path.length());
-                generate(res, s, target,j + 1, -num, value - num, path.append("-").append(num));
-                path.delete(cur, path.length());
-                generate(res,s, target, j + 1, pre * num, value - pre + pre * num, path.append("*").append(num));
-                path.delete(cur, path.length());
+                generate(res, s, target,j + 1, num, value + num, path + "+" + num);
+                generate(res, s, target,j + 1, -num, value - num, path + "-" + num);
+                generate(res,s, target, j + 1, pre * num, value - pre + pre * num, path + "*" + num);
             }
         }
     }
