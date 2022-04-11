148ea9508b6858ff548e326ea43fcf159151031b
Manh Nguyen <tienmanhbmt@gmail.com>
2022-04-11 11:55:16 +0700

    Time: 117 ms (55.79%), Space: 71.7 MB (81.49%) - LeetHub

--- 384-shuffle-an-array/384-shuffle-an-array.java
+++ 384-shuffle-an-array/384-shuffle-an-array.java
@@ -2,9 +2,11 @@ class Solution {
 
     int[] ori;
     int[] array;
+    Random rand;
     public Solution(int[] nums) {
         ori = nums;
         array = nums.clone();
+        rand = new Random();
     }
     
     /** Resets the array to its original configuration and return it. */
@@ -15,7 +17,7 @@ class Solution {
     /** Returns a random shuffling of the array. */
     public int[] shuffle() {
         for (int index = 0; index < array.length; index++) {
-            int a = ThreadLocalRandom.current().nextInt(0, array.length);
+            int a = rand.nextInt(0, array.length);
             int temp = array[index];
             array[index] = array[a];
             array[a] = temp;
