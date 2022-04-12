43c4829ce60a274fc621b0046fb9f8630691ed08
Manh Nguyen <tienmanhbmt@gmail.com>
2022-04-12 13:07:57 +0700

    Time: 88 ms (32.88%), Space: 73.7 MB (99.69%) - LeetHub

---
+++ 354-russian-doll-envelopes/354-russian-doll-envelopes.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int maxEnvelopes(int[][] envelopes) {
+        Arrays.sort(envelopes, (a,b) -> {
+            if (a[0] == b[0]) {
+                return b[1] - a[1];
+            } else {
+                return a[0] - b[0];
+            }
+        });
+        int max = 0;
+        int[] dp = new int[envelopes.length];
+        for (int i = 0; i < envelopes.length; i++) {
+            int l = 0;
+            int r = max;
+            while (l < r) {
+                //3 7 8 6
+                int m = l + (r - l) / 2;
+                if (envelopes[i][1] > dp[m]) {
+                    l = m + 1;
+                } else {
+                    r = m;
+                }
+            }
+            dp[l] = envelopes[i][1];
+            if (l == max)
+                max++;
+        }
+        return max;
+    }
+}
