6aaf825ac0191fde54a4057c95d87317bc6f76c1
Manh Nguyen <tienmanhbmt@gmail.com>
2022-07-03 21:59:19 +0700

    Time: 0 ms (100.00%), Space: 41.2 MB (74.67%) - LeetHub

---
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int wiggleMaxLength(int[] nums) {
+        /*
+        cannot use sliding window because it's subsequence not subarray
+        1 7 4 9 2 5
+                  1
+        5 2 9 4 7 1
+        1 2
+            2
+        */
+        if (nums.length < 2)
+            return nums.length;
+        int prevdiff = nums[1] - nums[0];
+        int count = prevdiff != 0 ? 2 : 1;
+        for (int i = 2; i < nums.length; i++) {
+            int diff = nums[i] - nums[i - 1];
+            if ((diff > 0 && prevdiff <= 0) || (diff < 0 && prevdiff >= 0)) {
+                count++;
+                prevdiff = diff;
+            }
+        }
+        return count;
+    }
+}
